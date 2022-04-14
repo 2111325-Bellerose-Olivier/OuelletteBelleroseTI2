e8a40f4438cff26728ecddb14d2e54f7dd0eaf0e
Antoine Ouellette <2143781@carrefour.cegepvicto.ca>
2022-04-14 14:21:51 -0400

    ajout du fichier SelectionDonnees.sql

---
+++ SelectionDonnees.sql
@@ -0,0 +1,10 @@
+/*
+ * Code de selection de données sur la bd pour Donjon Inc.
+ *
+ * Fichier : SelectionDonnees.sql
+ * Auteur : Olivier Belrose et Antoine Ouellette
+ * Langage : SQL
+ * Date : avril 2022
+ */
+ 
+USE RessourcesMonstrueuses;
