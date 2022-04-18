8b8e22d7ace4a7532b2c7d2079e25cd456503ac6
Antoine Ouellette <2143781@carrefour.cegepvicto.ca>
2022-04-18 17:23:28 -0400

    codé les fonctions crypter() et decrypter()

--- Fonctions.sql
+++ Fonctions.sql
@@ -9,3 +9,47 @@
  
 USE RessourcesMonstrueuses;
 
+DROP FUNCTION IF EXISTS crypter;
+DELIMITER $$
+CREATE FUNCTION crypter(_donnee VARCHAR(255)) RETURNS BLOB CONTAINS SQL DETERMINISTIC
+BEGIN
+	DECLARE _texte_crypte BLOB;
+	#Crypter _donnee avec cle 'mortauxheros'
+	SET @_texte_crypte = AES_ENCRYPT(_donnee, UNHEX(SHA2('mortauxheros', 256)));
+
+	RETURN @_texte_crypte;
+END $$
+DELIMITER ;
+
+
+DROP FUNCTION IF EXISTS decrypter;
+DELIMITER $$
+CREATE FUNCTION decrypter(_donnee_cryptee BLOB) RETURNS VARCHAR(255) NO SQL DETERMINISTIC
+BEGIN
+	DECLARE _texte_clair BLOB;
+	#Decrypter _donnee_cryptee avec cle 'mortauxheros'
+	SET @_texte_clair = AES_DECRYPT(_donnee_cryptee, UNHEX(SHA2('mortauxheros', 256)));
+
+	RETURN @_texte_clair;
+END $$
+DELIMITER ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
