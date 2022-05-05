54cbde9b9b28aac63c7aa506aa9742c1673f16a9
Oli <2111325@etudiant.cegepvicto.ca>
2022-05-05 10:16:11 -0400

    COMMIT PUSH pour trigger Hashage
    trigger hashage et ajout de documentation pour les triggers

--- Declencheurs.sql
+++ Declencheurs.sql
@@ -13,8 +13,11 @@ END $$
 DELIMITER ;
 
 
-
+/**
 # Validation Coffre 1 : update
+*
+*@Dependencies Ligne_coffre
+*/
 DROP TRIGGER IF EXISTS validation_coffre_update;
 DELIMITER $$
 CREATE TRIGGER validation_coffre_update BEFORE UPDATE ON Ligne_coffre FOR EACH ROW
@@ -64,7 +67,11 @@ BEGIN
 END; $$
 DELIMITER ;
 
+/**
 # Declencheur mortalite : update
+*
+*@Dependencies Affectation_salle
+*/
 DROP TRIGGER IF EXISTS declencheur_mortalite;
 DELIMITER $$
 CREATE TRIGGER declencheur_mortalite AFTER UPDATE ON Monstre FOR EACH ROW
@@ -79,7 +86,11 @@ BEGIN
 END; $$
 DELIMITER ;
 
+/**
 # Declencher éléments opposé:  insert
+*
+*@Dependencies Affectation_salle, Elementaire
+*/
 DROP TRIGGER IF EXISTS elements_oppose;
 DELIMITER $$
 CREATE TRIGGER elements_oppose AFTER INSERT ON Affectation_salle FOR EACH ROW
@@ -92,18 +103,35 @@ BEGIN
     SET _salle = (SELECT salle FROM Affectation_salle);
     SET _temps_debut = (SELECT debut_affectation FROM Affectation_salle);
     SET _temps_fin = (SELECT fin_affectation FROM Affectation_salle);
+    
     SET _verification = (SELECT elements_opposes_piece(_salle,_temps_debut,_temps_fin));
     
     IF _verification = 1 THEN
 		SIGNAL SQLSTATE '01002' SET MESSAGE_TEXT = 'Il y a un élémentaire d\'eau et de feu d\'assigné à la même salle.';
-	ELSE 
-		RETURN _verification;
 	END IF;
     
 END $$
 DELIMITER ;
 
+/**
+* déclencheur hashage
+* 
+*@Dependencies Coffre_tresor
+*/
+DROP TRIGGER IF EXISTS hashage;
+DELIMITER $$
+CREATE TRIGGER hashage BEFORE INSERT ON Coffre_tresor FOR EACH ROW
+BEGIN
+	DECLARE _code CHAR(64);
+    
+    SET _code = (SELECT code_secret FROM Coffre_tresor);
 
+     SET NEW.code_secret = SHA2(_code,256);
+    
+END $$
+DELIMITER ;
+
+         
 
 
 
